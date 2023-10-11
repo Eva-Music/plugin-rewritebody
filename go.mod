6d021a770ac7fc8b7d32c310773e8f788c661b75
Ольга Машкова <mashkova.o.i@sberbank.ru>
2023-10-11 14:48:29 +0300

    first commit

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/traefik/plugin-rewritebody
+module github.com/Eva-Music/plugin-rewritebody
 
 go 1.15
--- rewritebody.go
+++ rewritebody.go
@@ -5,69 +5,45 @@ import (
 	"bufio"
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
-	"regexp"
 )
 
 // Rewrite holds one rewrite body configuration.
-type Rewrite struct {
-	Regex       string `json:"regex,omitempty"`
-	Replacement string `json:"replacement,omitempty"`
+type Config struct {
+	obj string `json:"obj,omitempty"`
 }
 
 // Config holds the plugin configuration.
-type Config struct {
-	LastModified bool      `json:"lastModified,omitempty"`
-	Rewrites     []Rewrite `json:"rewrites,omitempty"`
-}
+//type Config struct {
+//	LastModified bool      `json:"lastModified,omitempty"`
+//	Rewrites     []Rewrite `json:"rewrites,omitempty"`
+//}
 
 // CreateConfig creates and initializes the plugin configuration.
 func CreateConfig() *Config {
 	return &Config{}
 }
 
-type rewrite struct {
-	regex       *regexp.Regexp
-	replacement []byte
-}
-
 type rewriteBody struct {
-	name         string
-	next         http.Handler
-	rewrites     []rewrite
-	lastModified bool
+	next   http.Handler
+	config Config
 }
 
 // New creates and returns a new rewrite body plugin instance.
-func New(_ context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	rewrites := make([]rewrite, len(config.Rewrites))
-
-	for i, rewriteConfig := range config.Rewrites {
-		regex, err := regexp.Compile(rewriteConfig.Regex)
-		if err != nil {
-			return nil, fmt.Errorf("error compiling regex %q: %w", rewriteConfig.Regex, err)
-		}
-
-		rewrites[i] = rewrite{
-			regex:       regex,
-			replacement: []byte(rewriteConfig.Replacement),
-		}
-	}
-
+func New(_ context.Context, next http.Handler, config *Config, _ string) (http.Handler, error) {
 	return &rewriteBody{
-		name:         name,
-		next:         next,
-		rewrites:     rewrites,
-		lastModified: config.LastModified,
+		next:   next,
+		config: *config,
 	}, nil
 }
 
 func (r *rewriteBody) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+
 	wrappedWriter := &responseWriter{
-		lastModified:   r.lastModified,
 		ResponseWriter: rw,
 	}
 
@@ -75,23 +51,25 @@ func (r *rewriteBody) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 
 	bodyBytes := wrappedWriter.buffer.Bytes()
 
-	contentEncoding := wrappedWriter.Header().Get("Content-Encoding")
+	//contentEncoding := wrappedWriter.Header().Get("Content-Encoding")
+	//
+	//if contentEncoding != "" && contentEncoding != "identity" {
+	//	if _, err := rw.Write(bodyBytes); err != nil {
+	//		log.Printf("unable to write body: %v", err)
+	//	}
+	//
+	//	return
+	//}
 
-	if contentEncoding != "" && contentEncoding != "identity" {
-		if _, err := rw.Write(bodyBytes); err != nil {
-			log.Printf("unable to write body: %v", err)
-		}
-
-		return
-	}
-
-	for _, rwt := range r.rewrites {
-		bodyBytes = rwt.regex.ReplaceAll(bodyBytes, rwt.replacement)
-	}
-
-	if _, err := rw.Write(bodyBytes); err != nil {
+	var resp map[string]interface{}
+	err := json.Unmarshal(bodyBytes, &resp)
+	if err != nil {
 		log.Printf("unable to write rewrited body: %v", err)
 	}
+
+	fmt.Println(resp[r.config.obj])
+	jsonResp, _ := json.Marshal(resp[r.config.obj])
+	rw.Write(jsonResp)
 }
 
 type responseWriter struct {
